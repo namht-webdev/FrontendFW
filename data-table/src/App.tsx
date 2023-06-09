7cc5a70179602b877d15eb3004d8436ad983686e
namht-webdev <namitus.hcmus@gmail.com>
2023-06-09 15:38:44 +0700

    data-table FW

---
+++ data-table/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center">
+      <header className="text-red-600 font-bold text-2xl uppercase pt-12">
+        Welcome to my initial config for applications
+      </header>
+    </div>
+  );
+}
+
+export default App;
